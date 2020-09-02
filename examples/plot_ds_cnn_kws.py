5fc31828d48c0c6d4f0e8f880300b0ebcdc65437
ktsiknos <ktsiknos@brainchip.com>
2020-09-02 14:36:33 +0200

    refactor(plot_ds_cnn_kws): update example with latest model
    In akida_models 1.0.6, ds_cnn models were refactored for better consistency
    resulting in a NSoC compatible model that can be used for both inference and
    edge learning (not shown in this example).
    
    This work adapts the existing example dataset and descriptions to fit to the new
    model.

--- examples/plot_ds_cnn_kws.py
+++ examples/plot_ds_cnn_kws.py
@@ -3,7 +3,7 @@ DS-CNN/KWS inference
 =======================
 
 This tutorial illustrates how to build a basic speech recognition
-Akida network that recognizes ten different words.
+Akida network that recognizes thirty different words.
 
 The model will be first defined as a CNN and trained in Keras, then
 converted using the `CNN2SNN toolkit <../user_guide/cnn2snn.html>`__.
@@ -21,79 +21,35 @@ image recognition tasks.
 """
 
 ######################################################################
-# 1. Load CNN2SNN tool dependencies
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-
-# System imports
-import os
-import sys
-import numpy as np
-import pickle
-from sklearn.metrics import accuracy_score
-import itertools
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-
-# TensorFlow imports
-import tensorflow.keras.backend as K
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.utils import get_file
-
-# KWS model imports
-from akida_models import ds_cnn_kws_pretrained
-
-######################################################################
-# 2. Load the preprocessed dataset
+# 1. Load the preprocessed dataset
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
+import pickle
 
-wanted_words = [
-    'down', 'go', 'left', 'no', 'off', 'on', 'right', 'stop', 'up', 'yes'
-]
-all_words = ['_silence_', '_unknown_'] + wanted_words
+from tensorflow.keras.utils import get_file
+
+# Fetch pre-processed data for 32 keywords
+fname = get_file(
+    fname='kws_preprocessed_all_words_except_backward_follow_forward.pkl',
+    origin=
+    "http://data.brainchip.com/dataset-mirror/kws/kws_preprocessed_all_words_except_backward_follow_forward.pkl",
+    cache_subdir='datasets/kws')
+with open(fname, 'rb') as f:
+    [_, _, x_valid_akida, y_valid, _, _, word_to_index, _] = pickle.load(f)
 
 # Preprocessed dataset parameters
-CHANNELS = 1
-CLASSES = len(all_words)
-SPECTROGRAM_LENGTH = 49
-FINGERPRINT_WIDTH = 10
+num_classes = len(word_to_index)
 
-input_shape = (SPECTROGRAM_LENGTH, FINGERPRINT_WIDTH, CHANNELS)
+print("Wanted words and labels:\n", word_to_index)
 
-# Try to load pre-processed dataset
-fname = get_file(
-    "preprocessed_data.pkl",
-    "http://data.brainchip.com/dataset-mirror/kws/preprocessed_data.pkl",
-    cache_subdir='datasets/kws')
-if os.path.isfile(fname):
-    print('Re-loading previously preprocessed dataset...')
-    f = open(fname, 'rb')
-    [x_train, y_train, x_valid, y_valid, train_files, val_files,
-     word_to_index] = pickle.load(f)
-    f.close()
-else:
-    raise ValueError("Unable to load the pre-processed KWS dataset.")
+# For cnn2snn Keras training, data must be scaled (usually to [0,1])
+a = 255
+b = 0
 
-# Transform the data to uint8
-x_train_min = x_train.min()
-x_train_max = x_train.max()
-max_int_value = 255.0
-
-# For akida hardware training and validation range [0, 255] inclusive uint8
-x_train_akida = ((x_train - x_train_min) * max_int_value /
-                 (x_train_max - x_train_min)).astype(np.uint8)
-x_valid_akida = ((x_valid - x_train_min) * max_int_value /
-                 (x_train_max - x_train_min)).astype(np.uint8)
-
-# For cnn2snn training and validation range [0,1] inclusive float32
-x_train_rescaled_cnn = (x_train_akida.astype(np.float32)) / max_int_value
-x_valid_rescaled_cnn = (x_valid_akida.astype(np.float32)) / max_int_value
-
-input_scaling = (max_int_value, 0)
+x_valid_keras = (x_valid_akida.astype('float32') - b) / a
 
 ######################################################################
-# 3. Create a Keras model satisfying Akida NSoC requirements
+# 2. Create a Keras model satisfying Akida NSoC requirements
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # The model consists of:
@@ -101,98 +57,109 @@ input_scaling = (max_int_value, 0)
 # * a first Convolutional layer accepting dense inputs (images),
 # * several Separable Convolutional layers preserving spatial dimensions,
 # * a global pooling reducing the spatial dimensions to a single pixel,
-# * a last Separable Convolutional layer to reduce the number of outputs
-#   to the number of words to predict.
+# * a last Separable Convolutional to reduce the number of outputs
+# * a final FullyConnected layer to classify words
 #
-# All layers are followed by a batch normalization and a ReLU activation,
-# except the last one that is followed by a SoftMax.
+# All layers are followed by a batch normalization and a ReLU activation.
 #
 # The first convolutional layer uses 8 bits weights, but other layers use
 # 4 bits weights.
 #
-# All activations are 4 bits.
+# All activations are 4 bits except for the final Separable Convolutional that
+# uses binary activations.
 #
-# .. Note:: The reason why we do not use a simple FullyConnected layer as the
-#           last layer is precisely because of the 4 bits activations, that are
-#           only supported as inputs by the Separable Convolutional layers.
+# Pre-trained weights were obtained after a few training episodes:
 #
-# Pre-trained weights were obtained after three training episodes:
-#
-# * first, we train the model with unconstrained float weights and
-#   activations for 30 epochs,
-# * then, we train the model with quantized activations only, with
-#   weights initialized from those trained in the previous episode,
-# * finally, we train the model with quantized weights and activations,
-#   with weights initialized from those trained in the previous episode.
+# * first, we train the model with unconstrained float weights and activations
+#   for 16 epochs,
+# * then, we train the model with quantized activations only, with weights
+#   initialized from those trained in the previous episode,
+# * then, we train the model with quantized weights, with both weights and
+#   activations initialized from those trained in the previous episode,
+# * finally, we train the model with quantized weights and activations and by
+#   gradually increasing quantization in the last layer.
 #
 # The table below summarizes the results obtained when preparing the
 # weights stored under `<http://data.brainchip.com/models/ds_cnn/>`__ :
 #
-# +---------+----------------+---------------+----------+--------+
-# | Episode | Weights Quant. | Activ. Quant. | Accuracy | Epochs |
-# +=========+================+===============+==========+========+
-# | 1       | N/A            | N/A           | 91.98 %  | 30     |
-# +---------+----------------+---------------+----------+--------+
-# | 2       | N/A            | 4 bits        | 92.13 %  | 30     |
-# +---------+----------------+---------------+----------+--------+
-# | 3       | 8/4 bits       | 4 bits        | 91.67 %  | 30     |
-# +---------+----------------+---------------+----------+--------+
+# +---------+----------------+----------------------------+----------+--------+
+# | Episode | Weights Quant. | Activ. Quant. / last layer | Accuracy | Epochs |
+# +=========+================+============================+==========+========+
+# | 1       | N/A            | N/A                        | 93.35 %  | 16     |
+# +---------+----------------+----------------------------+----------+--------+
+# | 2       | N/A            | 4 bits / 4 bits            | 92.18 %  | 16     |
+# +---------+----------------+----------------------------+----------+--------+
+# | 3       | 8/4 bits       | 4 bits / 4 bits            | 91.92 %  | 16     |
+# +---------+----------------+----------------------------+----------+--------+
+# | 4       | 8/4 bits       | 4 bits / 3 bits            | 92.29 %  | 16     |
+# +---------+----------------+----------------------------+----------+--------+
+# | 5       | 8/4 bits       | 4 bits / 2 bits            | 92.31 %  | 16     |
+# +---------+----------------+----------------------------+----------+--------+
+# | 6       | 8/4 bits       | 4 bits / 1 bit             | 92.53 %  | 16     |
+# +---------+----------------+----------------------------+----------+--------+
 #
 
-K.clear_session()
+from akida_models import ds_cnn_kws_pretrained
 model_keras = ds_cnn_kws_pretrained()
 model_keras.summary()
 
 ######################################################################
-# 4. Check performance
+# 3. Check performance
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import numpy as np
+
+from sklearn.metrics import accuracy_score
 
 # Check Model performance
-potentials_keras = model_keras.predict(x_valid_rescaled_cnn)
+potentials_keras = model_keras.predict(x_valid_keras)
 preds_keras = np.squeeze(np.argmax(potentials_keras, 1))
 
 accuracy = accuracy_score(y_valid, preds_keras)
 print("Accuracy: " + "{0:.2f}".format(100 * accuracy) + "%")
 
 ######################################################################
-# 5. Conversion to Akida
+# 4. Conversion to Akida
 # ~~~~~~~~~~~~~~~~~~~~~~
 #
-# 5.1 Convert the trained Keras model to Akida
+# 4.1 Convert the trained Keras model to Akida
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# We convert the model to Akida and verify that it is compatible with the
-# Akida NSoC (**HW** column in summary).
+# We convert the model to Akida and verify that it is compatible with Akida
+# NSoC.
 #
 
 # Convert the model
 from cnn2snn import convert
 
-model_akida = convert(model_keras, input_scaling=input_scaling)
+model_akida = convert(model_keras, input_scaling=(a, b))
 model_akida.summary()
 
+# Check comptability
+from akida.compatibility import model_hardware_incompatibilities
+model_hardware_incompatibilities(model_akida)
+
 ######################################################################
-# 5.2 Check prediction accuracy
+# 4.2 Check prediction accuracy
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 
-preds_akida = model_akida.predict(x_valid_akida, num_classes=CLASSES)
+preds_akida = model_akida.predict(x_valid_akida, num_classes=num_classes)
 
 accuracy = accuracy_score(y_valid, preds_akida)
 print("Accuracy: " + "{0:.2f}".format(100 * accuracy) + "%")
 
 # For non-regression purpose
-assert accuracy > 0.83
+assert accuracy > 0.9
 
 # Print model statistics
 print("Model statistics")
 stats = model_akida.get_statistics()
-model_akida.predict(x_valid_akida[:20], num_classes=CLASSES)
+model_akida.predict(x_valid_akida[:20], num_classes=num_classes)
 for _, stat in stats.items():
     print(stat)
 
 ######################################################################
-# 5.3 Confusion matrix
+# 4.3 Confusion matrix
 # ^^^^^^^^^^^^^^^^^^^^
 #
 # The confusion matrix provides a good summary of what mistakes the
@@ -206,29 +173,30 @@ for _, stat in stats.items():
 # recognition <https://github.com/tensorflow/docs/blob/master/site/en/r1/tutorials/sequences/audio_recognition.md#confusion-matrix>`__
 # example for a detailed explaination of the confusion matrix.
 #
+import itertools
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
 
 # Create confusion matrix
-label_mapping = dict(zip(all_words, range(len(all_words))))
-
-cm = confusion_matrix(y_valid, preds_akida, list(label_mapping.values()))
+cm = confusion_matrix(y_valid, preds_akida, list(word_to_index.values()))
 
 # Normalize
 cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
 # Display confusion matrix
-plt.rcParams["figure.figsize"] = (8, 8)
+plt.rcParams["figure.figsize"] = (16, 16)
 plt.figure()
 
-classes = label_mapping
 title = 'Confusion matrix'
 cmap = plt.cm.Blues
 
 plt.imshow(cm, interpolation='nearest', cmap=cmap)
 plt.title(title)
 plt.colorbar()
-tick_marks = np.arange(len(classes))
-plt.xticks(tick_marks, classes, rotation=45)
-plt.yticks(tick_marks, classes)
+tick_marks = np.arange(len(word_to_index))
+plt.xticks(tick_marks, word_to_index, rotation=45)
+plt.yticks(tick_marks, word_to_index)
 
 thresh = cm.max() / 2.
 for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
