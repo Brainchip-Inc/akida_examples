a552fac2e35e2271aa1854336516ccde0582df30
ktsiknos <ktsiknos@brainchip.com>
2020-10-12 12:13:06 +0200

    refactor(plot_ds_cnn_kws): update example architecture
    Changes fit the new architecture that was already implemented for CIFAR-10
    and UTKFace examples.

--- examples/plot_ds_cnn_kws.py
+++ examples/plot_ds_cnn_kws.py
@@ -3,7 +3,7 @@ DS-CNN/KWS inference
 =======================
 
 This tutorial illustrates how to build a basic speech recognition
-Akida network that recognizes thirty different words.
+Akida network that recognizes thirty-two different words.
 
 The model will be first defined as a CNN and trained in Keras, then
 converted using the `CNN2SNN toolkit <../user_guide/cnn2snn.html>`__.
@@ -49,19 +49,54 @@ b = 0
 x_valid_keras = (x_valid_akida.astype('float32') - b) / a
 
 ######################################################################
-# 2. Create a Keras model satisfying Akida NSoC requirements
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2. Load a pre-trained native Keras model
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # The model consists of:
 #
-# * a first Convolutional layer accepting dense inputs (images),
-# * several Separable Convolutional layers preserving spatial dimensions,
+# * a first convolutional layer accepting dense inputs (images),
+# * several separable convolutional layers preserving spatial dimensions,
 # * a global pooling reducing the spatial dimensions to a single pixel,
-# * a last Separable Convolutional to reduce the number of outputs
-# * a final FullyConnected layer to classify words
+# * a last separable convolutional to reduce the number of outputs
+# * a final fully connected layer to classify words
 #
 # All layers are followed by a batch normalization and a ReLU activation.
 #
+# This model was obtained with unconstrained float weights and activations after
+# 16 epochs of training.
+#
+
+from tensorflow.keras.models import load_model
+
+# Retrieve the model file from the BrainChip data server
+model_file = get_file("ds_cnn_kws.h5",
+                      "http://data.brainchip.com/models/ds_cnn/ds_cnn_kws.h5",
+                      cache_subdir='models')
+
+# Load the native Keras pre-trained model
+model_keras = load_model(model_file)
+model_keras.summary()
+
+######################################################################
+
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+
+# Check Keras Model performance
+potentials_keras = model_keras.predict(x_valid_keras)
+preds_keras = np.squeeze(np.argmax(potentials_keras, 1))
+
+accuracy = accuracy_score(y_valid, preds_keras)
+print("Accuracy: " + "{0:.2f}".format(100 * accuracy) + "%")
+
+######################################################################
+# 3. Load a pre-trained quantized Keras model satisfying Akida NSoC requirements
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# The above native Keras model is quantized and fine-tuned to get a quantized
+# Keras model satisfying the `Akida NSoC requirements
+# <https://doc.brainchipinc.com/user_guide/hw_constraints.html>`__.
 # The first convolutional layer uses 8 bits weights, but other layers use
 # 4 bits weights.
 #
@@ -70,10 +105,8 @@ x_valid_keras = (x_valid_akida.astype('float32') - b) / a
 #
 # Pre-trained weights were obtained after a few training episodes:
 #
-# * first, we train the model with unconstrained float weights and activations
-#   for 16 epochs,
-# * then, we train the model with quantized activations only, with weights
-#   initialized from those trained in the previous episode,
+# * we train the model with quantized activations only, with weights initialized
+#   from those trained in the previous episode (native Keras model),
 # * then, we train the model with quantized weights, with both weights and
 #   activations initialized from those trained in the previous episode,
 # * finally, we train the model with quantized weights and activations and by
@@ -100,49 +133,35 @@ x_valid_keras = (x_valid_akida.astype('float32') - b) / a
 #
 
 from akida_models import ds_cnn_kws_pretrained
-model_keras = ds_cnn_kws_pretrained()
-model_keras.summary()
 
-######################################################################
-# 3. Check performance
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import numpy as np
-
-from sklearn.metrics import accuracy_score
+# Load the pre-trained quantized model
+model_keras_quantized = ds_cnn_kws_pretrained()
+model_keras_quantized.summary()
 
 # Check Model performance
-potentials_keras = model_keras.predict(x_valid_keras)
-preds_keras = np.squeeze(np.argmax(potentials_keras, 1))
+potentials_keras_q = model_keras_quantized.predict(x_valid_keras)
+preds_keras_q = np.squeeze(np.argmax(potentials_keras_q, 1))
 
-accuracy = accuracy_score(y_valid, preds_keras)
-print("Accuracy: " + "{0:.2f}".format(100 * accuracy) + "%")
+accuracy_q = accuracy_score(y_valid, preds_keras_q)
+print("Accuracy: " + "{0:.2f}".format(100 * accuracy_q) + "%")
 
 ######################################################################
 # 4. Conversion to Akida
 # ~~~~~~~~~~~~~~~~~~~~~~
 #
-# 4.1 Convert the trained Keras model to Akida
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#
-# We convert the model to Akida and verify that it is compatible with Akida
-# NSoC.
+# We convert the model to Akida and then evaluate the performances on the
+# dataset.
 #
 
-# Convert the model
 from cnn2snn import convert
 
-model_akida = convert(model_keras, input_scaling=(a, b))
+# Convert the model
+model_akida = convert(model_keras_quantized, input_scaling=(a, b))
 model_akida.summary()
 
-# Check comptability
-from akida.compatibility import model_hardware_incompatibilities
-model_hardware_incompatibilities(model_akida)
-
 ######################################################################
-# 4.2 Check prediction accuracy
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#
 
+# Check Akida model performance
 preds_akida = model_akida.predict(x_valid_akida, num_classes=num_classes)
 
 accuracy = accuracy_score(y_valid, preds_akida)
@@ -150,6 +169,8 @@ print("Accuracy: " + "{0:.2f}".format(100 * accuracy) + "%")
 
 # For non-regression purpose
 assert accuracy > 0.9
+
+######################################################################
 
 # Print model statistics
 print("Model statistics")
@@ -159,7 +180,7 @@ for _, stat in stats.items():
     print(stat)
 
 ######################################################################
-# 4.3 Confusion matrix
+# 5. Confusion matrix
 # ^^^^^^^^^^^^^^^^^^^^
 #
 # The confusion matrix provides a good summary of what mistakes the
@@ -171,7 +192,7 @@ for _, stat in stats.items():
 #
 # Please refer to the Tensorflow `audio
 # recognition <https://github.com/tensorflow/docs/blob/master/site/en/r1/tutorials/sequences/audio_recognition.md#confusion-matrix>`__
-# example for a detailed explaination of the confusion matrix.
+# example for a detailed explanation of the confusion matrix.
 #
 import itertools
 import matplotlib.pyplot as plt
