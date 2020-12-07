24356fa2e1c0198ee35a26de641bdc373aba39af
ktsiknos <ktsiknos@brainchip.com>
2020-12-07 16:00:44 +0100

    refactor(plot_edge_learning_vision): homogenize example with others

--- examples/plot_edge_learning_vision.py
+++ examples/plot_edge_learning_vision.py
@@ -2,39 +2,33 @@
 Akida vision edge learning
 ==========================
 
-Below is not a tutorial but just a code snippet for vision. Please refer to
-the `keyword spotting (KWS) tutorial <plot_edge_learning_kws.html>`__ for edge
-learning documentation, parameters fine tuning and steps details.
+This tutorial demonstrates the Akida NSoC **edge learning** capabilities using
+its built-in learning algorithm.
+It focuses on an image classification example, where an existing Akida network
+is re-trained to be able to classify images from 5 new classes.
 
-Note that unlike for the `KWS <plot_edge_learning_kws.html>`__ there is no
-"offline" Akida training.
+Just a few samples (few-shot learning) of the new classes are sufficient
+to augment the Akida model with extra classes, while preserving high accuracy.
+
+Please refer to the `keyword spotting (KWS) tutorial <plot_edge_learning_kws.html>`__
+for edge learning documentation, parameters fine tuning and steps details.
+
 """
 
 ##############################################################################
-# 1. Load `coil100 <https://www.tensorflow.org/datasets/catalog/coil100>`__ dataset
-# ---------------------------------------------------------------------------------
+# 1. Dataset preparation
+# ~~~~~~~~~~~~~~~~~~~~~~
 
-import matplotlib.pyplot as plt
-import numpy as np
-import statistics as stat
-import tensorflow as tf
 import tensorflow_datasets as tfds
 
-from time import time
-from tensorflow.keras.utils import get_file
-
-from akida import FullyConnected
-
-########################
-
-# Retrieve TensorFlow dataset
-tfds.disable_progress_bar()
+# Retrieve TensorFlow `coil100 <https://www.tensorflow.org/datasets/catalog/coil100>`__
+# dataset
 ds, ds_info = tfds.load('coil100:1.*.*', split='train', with_info=True)
 print(ds_info.description)
 
-########################
+##############################################################################
 
-# Select the 5 cup objects
+# Select the 5 cup objects that will be used as new classes
 object_ids = ['obj10', 'obj16', 'obj18', 'obj25', 'obj43']
 object_dict = {k: [] for k in object_ids}
 for data in ds:
@@ -42,7 +36,9 @@ for data in ds:
     if object_id in object_dict.keys():
         object_dict[object_id].append(data['image'].numpy())
 
-########################
+##############################################################################
+
+import matplotlib.pyplot as plt
 
 # Display one image per selected object
 f, axarr = plt.subplots(1, len(object_dict))
@@ -56,7 +52,7 @@ plt.show()
 
 ##############################################################################
 # 2. Prepare Akida model for learning
-# -----------------------------------
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from akida_models import mobilenet_edge_imagenet_pretrained
 from cnn2snn import convert
@@ -67,7 +63,9 @@ model_keras = mobilenet_edge_imagenet_pretrained()
 # Convert it to akida
 model_ak = convert(model_keras, input_scaling=(128, 128))
 
-########################
+##############################################################################
+
+from akida import FullyConnected
 
 # Replace the last layer by a classification layer
 num_classes = len(object_dict)
@@ -84,8 +82,13 @@ model_ak.compile(num_weights=num_weights,
 model_ak.summary()
 
 ##############################################################################
-# 4. Learn with Akida
-# -------------------
+# 3. Edge learning with Akida
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import numpy as np
+
+from tensorflow.image import resize_with_crop_or_pad
+from time import time
 
 # Learn objects in num_shots shot(s)
 num_shots = 1
@@ -93,14 +96,15 @@ for i in range(len(object_ids)):
     start = time()
     train_images = object_dict[object_ids[i]][:num_shots]
     for image in train_images:
-        padded_image = tf.image.resize_with_crop_or_pad(image, 224, 224)
+        padded_image = resize_with_crop_or_pad(image, 224, 224)
         model_ak.fit(np.expand_dims(padded_image, axis=0), i)
     end = time()
-    print(
-        f'Learned object {object_ids[i]} (class {i}) with {len(train_images)} sample(s) in {end-start:.2f}s'
-    )
+    print(f'Learned object {object_ids[i]} (class {i}) with \
+            {len(train_images)} sample(s) in {end-start:.2f}s')
 
-########################
+##############################################################################
+
+import statistics as stat
 
 # Check accuracy against remaining samples
 accuracy = []
@@ -108,15 +112,15 @@ for i in range(len(object_ids)):
     test_images = object_dict[object_ids[i]][num_shots:]
     predictions = np.zeros(len(test_images))
     for j in range(len(test_images)):
-        padded_image = tf.image.resize_with_crop_or_pad(test_images[j], 224,
-                                                        224)
+        padded_image = resize_with_crop_or_pad(test_images[j], 224, 224)
         predictions[j] = model_ak.predict(np.expand_dims(padded_image, axis=0),
                                           num_classes=num_classes)
     accuracy.append(100 * np.sum(predictions == i) / len(test_images))
-    print(
-        f'Accuracy testing object {object_ids[i]} (class {i}) with {len(test_images)} sample(s): {accuracy[i]:.2f}%'
-    )
+    print(f'Accuracy testing object {object_ids[i]} (class {i}) with \
+            {len(test_images)} sample(s): {accuracy[i]:.2f}%')
 
-print(f'Total accuracy: {stat.mean(accuracy):.2f}%')
+mean_accuracy = stat.mean(accuracy)
+print(f'Mean accuracy: {mean_accuracy:.2f}%')
+
 # For non-regression purpose
-assert stat.mean(accuracy) > 98
+assert mean_accuracy > 99
