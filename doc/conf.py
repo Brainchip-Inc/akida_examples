27be3fdd53f18ec85abd0e318abb866d789eea9f
ktsiknos <ktsiknos@brainchip.com>
2021-04-08 15:29:16 +0200

    chore(conf): clean copyright footer

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Akida Examples'
-copyright = 'Copyright 2020, BrainChip Holdings Ltd. All Rights Reserved.'
+copyright = '2020, BrainChip Holdings Ltd. All Rights Reserved'
 author = 'Brainchip'
 
 # The full version, including alpha/beta/rc tags
