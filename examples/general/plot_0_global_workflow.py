06efd1e6ce0f620754dc2cc5a884ce2cd6d10d5e
Richard Chevalier <rchevalier@brainchip.com>
2023-08-18 13:06:40 +0000

    chore(doc): Akida flow example update
    Signed-off-by: Richard Chevalier <rchevalier@brainchip.com>

--- examples/general/plot_0_global_workflow.py
+++ examples/general/plot_0_global_workflow.py
@@ -1,18 +1,21 @@
 """
-Global Akida workflow tutorial
-==============================
+Global Akida workflow
+=====================
 
-This tutorial illustrates how to use the QuantizeML and CNN2SNN toolkits to produce a model that can
-be used with Akida accelerator. You can refer to our `Akida user guide
-<../../user_guide/akida.html>`__ for further explanation.
+Using the MNIST dataset, this example shows the definition and training of a keras
+floating point model, its quantization to 8-bit with the help of calibration,
+its quantization to 4-bit using QAT and its conversion to Akida.
+Notice that the performance of the original keras floating point model is maintained
+throughout the Akida flow.
+Please refer to the `Akida user guide <../../user_guide/akida.html>`__ for further information.
 
-.. Note:: Please refer to TensorFlow  `tf.keras.models
+.. Note:: Please refer to the TensorFlow  `tf.keras.models
           <https://www.tensorflow.org/api_docs/python/tf/keras/models>`__
-          module for model creation/import details and `TensorFlow Guide
-          <https://www.tensorflow.org/guide>`__ for details of how TensorFlow works.
+          module for model creation/import details and the `TensorFlow Guide
+          <https://www.tensorflow.org/guide>`__ for TensorFlow usage.
 
-          MNIST example below is light enough so you do not need a `GPU
-          <https://www.tensorflow.org/install/gpu>`__ to run the training steps.
+          The MNIST example below is light enough so that a `GPU
+          <https://www.tensorflow.org/install/gpu>`__ is not needed for training.
 
 
 .. figure:: ../../img/overall_flow.png
@@ -44,8 +47,9 @@ from keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # Add a channels dimension to the image sets as Akida expects 4-D inputs (corresponding to
-# (num_samples, width, height, channels). Note: MNIST is unusual in this respect - most image data
-# already includes a channel dimension, and this step will not be necessary.
+# (num_samples, width, height, channels). Note: MNIST is a grayscale dataset and is unusual
+# in this respect - most image data already includes a channel dimension, and this step will
+# not be necessary.
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 
@@ -61,12 +65,9 @@ plt.show()
 # ^^^^^^^^^^^^^^^^^^^^^
 #
 # Note that at this stage, there is nothing specific to the Akida IP.
-# This start point is very much a completely standard CNN as defined
-# within `Keras <https://www.tensorflow.org/api_docs/python/tf/keras>`__.
-#
-# An appropriate model for MNIST (inspired by `this example
-# <https://www.tensorflow.org/model_optimization/guide/quantization/training_example#train_a_model_for_mnist_without_quantization_aware_training>`__)
-# might look something like the following:
+# The model constructed below, as inspired by `this example
+# <https://www.tensorflow.org/model_optimization/guide/quantization/training_example#train_a_model_for_mnist_without_quantization_aware_training>`__,
+# is a completely standard `Keras <https://www.tensorflow.org/api_docs/python/tf/keras>`__ CNN model.
 #
 
 import keras
@@ -91,8 +92,8 @@ model_keras.summary()
 # 1.3. Model training
 # ^^^^^^^^^^^^^^^^^^^
 #
-# Before going any further, train the model and get its performance. The created model should
-# achieve a test accuracy over 98% after 10 epochs.
+# Given the model created above, train the model and check its accuracy. The model should achieve
+# a test accuracy over 98% after 10 epochs.
 #
 from keras.optimizers import Adam
 
@@ -115,17 +116,17 @@ print('Test accuracy:', score[1])
 # 2.1. 8-bit quantization
 # ^^^^^^^^^^^^^^^^^^^^^^^
 #
-# We can now turn to quantization to get a discretized version of the model, where the weights and
-# activations are quantized so as to be suitable for conversion towards an Akida accelerator.
+# An Akida accelerator processes 8, 4, 2, or 1 bit integer activations and weights. Therefore,
+# the floating point Keras model must be quantized in preparation to run on an Akida accelerator.
 #
-# For this, we just have to quantize the Keras model using the QuantizeML
-# `quantize <../../api_reference/quantizeml_apis.html#quantizeml.models.quantize>`__
-# function. The selected quantization scheme is 8/8/8 which stands for 8-bit weights in the first
-# layer, 8-bit weights in other layers and 8-bit activations respectively.
+# The QuantizeML `quantize <../../api_reference/quantizeml_apis.html#quantizeml.models.quantize>`__
+# function can be used to quantize a Keras model for Akida. For this step in this example, an
+# “8/8/8” quantization scheme will be applied to the floating point Keras model to produce 8-bit
+# weights in the first layer, 8-bit weights in all other layers, and 8-bit activations.
 #
-# The quantized model is a Keras model where the layers are replaced with custom `QuantizeML
-# quantized layers <../../api_reference/quantizeml_apis.html#layers>`__. All Keras API functions
-# can be applied on this new model: ``summary()``, ``compile()``, ``fit()``. etc.
+# The quantization process results in a Keras model with custom `QuantizeML quantized layers
+# <../../api_reference/quantizeml_apis.html#layers>`__ substituted for the original Keras layers.
+# All Keras API functions can be applied on this new model: ``summary()``, ``compile()``, ``fit()``. etc.
 #
 # .. Note:: The ``quantize`` function applies `several transformations
 #           <../../api_reference/quantizeml_apis.html#transforms>`__ to
@@ -157,13 +158,15 @@ print('Test accuracy after 8-bit quantization:', compile_evaluate(model_quantize
 # 2.2. Effect of calibration
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# The previous call to ``quantize`` was made with random samples for calibration (default
-# parameters). While the observed accuracy drop is minimal, that is around 1%, it can be higher on
-# more complex models and it is advised to use a set of real samples from the training set. Note
-# that this remains a calibration rather than a training step: any relevant data could be used, and
-# crucially, no labels are required.
-# The recommended configuration for calibration that is widely used to obtain
-# `model zoo performance <../../model_zoo_performance.html#akida-2-0-models>`__ is:
+# The previous call to ``quantize`` was made with random samples for calibration
+# (default parameters). While the observed drop in accuracy is minimal, that is
+# around 1%, it can be worse on more complex models. Therefore, it is advised to
+# use a set of real samples from the training set for calibration during a call
+# to ``quantize``.
+# Note that this remains a calibration step rather than a training step in that
+# no output labels are required. Furthermore, any relevant data could be used for
+# calibration. The recommended settings for calibration that are widely used to
+# obtain the `zoo performance <../../model_zoo_performance.html#akida-2-0-models>`__ are:
 #
 # - 1024 samples
 # - a batch size of 100
@@ -173,28 +176,25 @@ model_quantized = quantize(model_keras, qparams=qparams,
                            samples=x_train, num_samples=1024, batch_size=100, epochs=2)
 
 ######################################################################
-# Check the quantized and calibrated with real samples accuracy.
+# Check the accuracy for the quantized and calibrated model.
 
 print('Test accuracy after calibration:', compile_evaluate(model_quantized))
 
 ######################################################################
-# Calibrating with real samples on this model allows to recover the initial float accuracy.
+# Calibrating with real samples on this model recovers the initial float accuracy.
 
 ######################################################################
 # 2.3. 4-bit quantization
 # ^^^^^^^^^^^^^^^^^^^^^^^
 #
-# The accuracy of the 8-bit quantized model is equivalent to the one of the base model. In this
-# section, a lower bitwidth quantization scheme that is still compatible with Akida accelerator is
-# adopted.
-# The accuracy of the 8-bit quantized model is equal to that of the base model. That quantized model
-# is already compatible with the Akida accelerator (following "conversion", see below), and for most
-# users, no further quantization is required. In a few cases, it may be attractive to bring the
-# model down to an even lower bitwidth quantization scheme, and here we show how to do that.
+# The accuracy of the 8/8/8 quantized model is equal to that of the Keras floating point
+# model. In some cases, a smaller memory size for the model is required. This can be
+# accomplished through quantization of the model to smaller bitwidths.
 #
-# The model will now be quantized to 8/4/4, that is 8-bit weights in the first layer and 4-bit
-# weights and activations everywhere else. Such a quantization scheme will usually introduce a
-# performance drop.
+# The model will now be quantized to 8/4/4, that is 8-bit weights in the first layer with
+# 4-bit weights and activations in all other layers. Such a quantization scheme will usually
+# introduce a performance drop.
+#
 
 qparams = QuantizationParams(input_weight_bits=8, weight_bits=4, activation_bits=4)
 model_quantized = quantize(model_keras, qparams=qparams,
@@ -209,8 +209,8 @@ print('Test accuracy after 4-bit quantization:', compile_evaluate(model_quantize
 # 2.4. Model fine tuning (quantization-aware training)
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# When a model suffers from a large accuracy drop after quantization, fine tuning or "quantization
-# aware training" (QAT) allows to recover some or all performance.
+# When a model suffers from an accuracy drop after quantization, fine tuning or “quantization
+# aware training” (QAT) may recover some or all of the original performance.
 #
 # Note that since this is a fine tuning step, both the number of epochs and learning rate are
 # expected to be lower than during the initial float training.
@@ -235,10 +235,9 @@ print('Test accuracy after fine tuning:', score)
 # 3.1 Convert to Akida model
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# When the quantized model achieves satisfactory performance, it can be converted to an Akida
-# accelerator suitable format. The
-# `convert <../../api_reference/cnn2snn_apis.html#cnn2snn.convert>`__ function returns a model in
-# Akida format ready for inference.
+# When the quantized model produces satisfactory performance, it can be converted to the native
+# Akida format. The `convert <../../api_reference/cnn2snn_apis.html#cnn2snn.convert>`__ function
+# returns a model in Akida format ready for inference.
 #
 # As with Keras, the summary() method provides a textual representation of the Akida model.
 #
@@ -262,8 +261,8 @@ assert accuracy > 0.96
 # 3.3 Show predictions for a single image
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# Now try processing a single image, say, the first image in the dataset
-# that we looked at above:
+# Display one of the test images, such as the first image in the dataset from above, to visualize
+# the output of the model.
 #
 
 # Test a single example
@@ -283,10 +282,10 @@ plt.show()
 print(outputs.squeeze())
 
 ######################################################################
-# Consider the output from the model, printed above. As is typical in backprop trained models, the
-# final layer here comprises a Dense layer, with one neuron per class in the dataset (here, 10). The
-# goal of training is to maximize the response of the neuron corresponding to the label of each
-# training sample, while minimizing the responses of the other neurons.
+# Consider the output from the model above. As is typical in backprop-trained models, the final
+# layer is a Dense layer with one neuron for each of the 10 classes in the dataset. The goal of
+# training is to maximize the response of the neuron corresponding to the label of each training
+# sample while minimizing the responses of the other neurons.
 #
 # In the bar chart above, you can see the outputs from all 10 neurons. It is easy to see that neuron
 # 7 responds much more strongly than the others. The first sample is indeed a number 7.
