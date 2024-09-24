23eb2a465738ddb2c107d028525a341e04757ef2
ktsiknos <ktsiknos@brainchip.com>
2024-09-24 11:22:24 +0200

    chore(ci): merge check device steps

--- ci/test_devices.sh
+++ ci/test_devices.sh
@@ -2,3 +2,4 @@
 
 set -e
 python -c "import akida; assert len(akida.devices()), 'Device not found.'"
+python -c "import tensorflow as tf; assert len(tf.config.list_physical_devices('GPU'))"
--- ci/test_gpu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-set -e
-python -c "import tensorflow as tf; assert len(tf.config.list_physical_devices('GPU'))"
