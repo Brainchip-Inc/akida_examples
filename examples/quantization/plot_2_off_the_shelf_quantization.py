37d094f911942677e2712843a5d640b20d730aa6
ktsiknos <ktsiknos@brainchip.com>
2025-10-29 10:39:05 +0100

    chore(plot_2_off_the_shelf_quantization): drop warning
    ONNX quantization has been proven for a while now.

--- examples/quantization/plot_2_off_the_shelf_quantization.py
+++ examples/quantization/plot_2_off_the_shelf_quantization.py
@@ -2,9 +2,6 @@
 Off-the-shelf models quantization
 =================================
 
-.. Warning::
-   QuantizeML ONNX quantization is an **evolving feature**. Some models may not be compatible.
-
 | The `Global Akida workflow <../general/plot_0_global_workflow.html>`__ and the
   `PyTorch to Akida workflow <../general/plot_8_global_pytorch_workflow.html>`__ guides
   describe all the steps required to create, train, quantize and convert a model for Akida,
